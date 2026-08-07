459ca47abf54d91cb077502e682587da6881588b
Joaquin2108 <j.monederosimon@student.maastrichtuniversity.nl>
2026-08-07 17:47:16 +0200

    Added basic UI elements

--- Day1/connection_setup.py
+++ Day1/connection_setup.py
@@ -19,3 +19,13 @@ version = session.sql("SELECT current_version()").collect()[0][0]
 
 #Green checkmark to indicate a successful connection
 st.success(f"Connected to Snowflake! Current version: {version}")
+
+# Streamlit UI elements. Text, headers, subheaders, captions, code blocks, and LaTeX can be displayed using Streamlit's built-in functions.
+st.title("Testing a Snowflake Connection in Streamlit")
+st.header("Day 1: Connection Setup")
+st.markdown("This is a markdown text. You can use **bold**, *italic*, and [links](https://www.snowflake.com/) in markdown.")
+st.subheader("This is the subheader.")
+st.caption("This is the caption. It is usually used for small print or disclaimers. ")
+st.code("x = 2021  \nprint(x)   # This is a code block. You can use it to display code snippets.")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+st.caption("Above is a LaTeX example. You can use LaTeX to display mathematical equations and symbols.")
